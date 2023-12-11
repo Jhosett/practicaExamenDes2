94658b2068fe1c115442604fbff116f65f30c168
Jhosett <darwinjhosettbermudezromero@gmail.com>
2023-12-11 17:03:25 -0500

    Iniciando práctica examen

---
+++ practicaExamenDes2/src/App.vue
@@ -0,0 +1,63 @@
+<script setup>
+
+</script>
+
+<template>
+  
+<div class="flex">
+  <div class="h-[600px] w-16 bg-slate-950">
+    <div class="py-1 mx-auto h-10 w-10">
+      <img class="rounded" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT5pjxeZ1dITxV1-a3e8AQPf9B4HXBg4P145g&usqp=CAU" alt="">
+    </div>
+    <p class="text-gray-500 text-center py-1">Perfil</p>
+  </div>
+
+  <div class="h-[600px] w-[400px] bg-slate-800">
+    <div class="ml-5 mt-4">
+
+      <p class="mb-5 text-gray-300">TENDENCIAS</p>
+
+      <div class="mb-12">
+        <P class="mb-2 text-gray-400">#titanic</P>
+        <P class="mb-2 text-gray-400">#millonarios</P>
+        <P class="mb-2 text-gray-400">FinalSemestre:D</P>
+        <P class="mb-2 text-gray-400">#UFC</P>
+        <P class="mb-2 text-gray-400">#ColombiaMarcha</P>
+        <P class="mb-2 text-gray-400">#UFPSO</P>
+      </div>
+
+      <p class="text-gray-300 mb-6">MENSAJES DIRECTOS</p>
+
+      <div>
+
+        <div class="flex bg-gray-700 mr-3 rounded items-center p-3 mb-2">
+          <img class="h-10 w-10 rounded-full ml-3" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLdHZMPuW6L7WYKZU0yds4Hyf8gfXCEGymuA&usqp=CAU" alt="">
+          <div class="ml-3 my-auto">
+            <p class="text-gray-300 text-base">Marcos Pérez</p>
+            <p class="text-gray-400 text-xs mt-[-6px]">Hace 10 minutos</p>
+          </div>
+        </div>
+
+        <div class="flex bg-gray-700 mr-3 rounded items-center p-3 mb-2">
+          <img class="h-10 w-10 rounded-full ml-3" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLdHZMPuW6L7WYKZU0yds4Hyf8gfXCEGymuA&usqp=CAU" alt="">
+          <div class="ml-3 my-auto">
+            <p class="text-gray-300 text-base">Marcos Pérez</p>
+            <p class="text-gray-400 text-xs mt-[-6px]">Hace 10 minutos</p>
+          </div>
+        </div>
+
+        <div class="flex bg-gray-700 mr-3 rounded items-center p-3 mb-2">
+          <img class="h-10 w-10 rounded-full ml-3" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLdHZMPuW6L7WYKZU0yds4Hyf8gfXCEGymuA&usqp=CAU" alt="">
+          <div class="ml-3 my-auto">
+            <p class="text-gray-300 text-base">Marcos Pérez</p>
+            <p class="text-gray-400 text-xs mt-[-6px]">Hace 10 minutos</p>
+          </div>
+        </div>
+
+      </div>
+
+    </div>
+  </div>
+</div>
+
+</template>
